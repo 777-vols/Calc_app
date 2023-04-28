5d4ea607ed78ba036c286508027fddc446d20324
Vladislav <samokhvalov.0202@mail.ru>
2023-04-28 13:44:34 +0300

    webpack_config

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import './index.scss'
+import ReactDOM from "react-dom/client";
+import React, { Component } from "react";
+import App from '../components/App';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	mode: "development",
+	entry: ["@babel/polyfill", "./src/index.jsx"],
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].[hash].js",
+	},
+	devServer: {
+		port: 3000,
+		hot: false
+	},
+	resolve: {
+		extensions: [".jsx", ".js", ".scss"],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Development',
+			template: "./src/index.html"
+		}),
+		new CleanWebpackPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"],
+			},
+			{
+				test: /\.(css|scss)$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(jpg|jpeg|png|svg)/,
+				use: ['file-loader']
+			},
+		]
+	}
+}
